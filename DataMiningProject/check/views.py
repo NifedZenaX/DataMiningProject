fa28cb85a46d9a35f84275739ed5e65094199096
NifedZenaX <soo.willie@yahoo.co.id>
2023-01-14 13:53:55 +0700

    fixed problem with model

--- DataMiningProject/check/views.py
+++ DataMiningProject/check/views.py
@@ -4,42 +4,40 @@ from django.http import HttpResponse
 import pickle
 import numpy as np
 
-class IGData(forms.Form):
-    user_length = forms.CharField(label="Username length")
-    user_has_num = forms.BooleanField(label="Does your username has any numbers", widget=forms.RadioSelect(choices=[(1, "Yes"), (2, "No")]),required=False)
-    name_length = forms.CharField(label="Full name length")
-    name_has_num = forms.BooleanField(label="Does your full name has any numbers", widget=forms.RadioSelect(choices=[(1, "Yes"), (0, "No")]),required=False)
-    is_private = forms.BooleanField(label="Is your account private", widget=forms.RadioSelect(choices=[(1, "Yes"), (0, "No")]),required=False)
-    recent_join = forms.BooleanField(label="Do you recently joined instagram", widget=forms.RadioSelect(choices=[(1, "Yes"), (0, "No")]),required=False)
-    is_business_acc = forms.BooleanField(label="Is this account a business account", widget=forms.RadioSelect(choices=[(1, "Yes"), (0, "No")]),required=False)
-
 # Create your views here.
 def check(request):
     predict = None
     if request.method == 'POST':
-        form = IGData(request.POST)
-        if form.is_valid():
-            data = [
-                int(form.cleaned_data['user_length']),
-                int(form.cleaned_data['user_has_num']),
-                int(form.cleaned_data['name_has_num']),
-                int(form.cleaned_data['name_length']),
-                int(form.cleaned_data['is_private']),
-                int(form.cleaned_data['recent_join']),
-                int(form.cleaned_data['is_business_acc'])
+        user_length = request.POST.get('user_length')
+        user_has_num = request.POST.get('user_has_num')
+        name_length = request.POST.get('name_length')
+        name_has_num = request.POST.get('name_has_num')
+        is_private = request.POST.get('is_private')
+        recent_join = request.POST.get('recent_join')
+        is_business_acc = request.POST.get('is_business_acc')
+        data = [
+                int(user_length),
+                int(user_has_num),
+                int(name_has_num),
+                int(name_length),
+                int(is_private),
+                int(recent_join),
+                int(is_business_acc)
             ]
-            predict = load_model([data])
-            print("prediction: " + str(predict))
+        predict = load_model([data])
+        print("prediction: " + str(predict))
     return render(request, 'check/predict.html', {'predict' : predict})
 
 def index(request):
-    form = IGData()
-    return render(request, 'check/index.html', {'form': form})
+    return render(request, 'check/index.html')
+
+model = None
 
 def load_model(data):
-    with open('D:\OneDrive - Bina Nusantara\Tugas2\Semester-5\Data Mining\model.pkl', 'rb') as f:
-        model = pickle.load(f)
-
+    global model
+    if model is None:
+        with open('check/model.pkl', 'rb') as f:
+            model = pickle.load(f)
     result = model.predict(data)[0]
 
     return result
